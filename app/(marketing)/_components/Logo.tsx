5e688be90c886d4dd2e2b87756ba41e36361ba44
Valeh Mammadli <valehlothbrok@gmail.com>
2024-02-12 21:43:42 +0400

    Folders setup & Landing page

---
+++ app/(marketing)/_components/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils"
+
+
+const font = Poppins({subsets: ["latin"],
+weight: ["400", "600"]})
+
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2"><Image alt="Logo" src="/logo.svg" width={40} height={40} />
+    </div>
+  )
+}
