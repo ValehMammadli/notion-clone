f8620dd53fa32a9ceca67c4bba2dfbf154ffce10
Valeh Mammadli <valehlothbrok@gmail.com>
2024-02-13 19:18:07 +0400

    Implement dark mode and authentication with Convex Clerk

--- app/(marketing)/_components/Logo.tsx
+++ app/(marketing)/_components/Logo.tsx
@@ -1,15 +1,28 @@
-import Image from "next/image"
-import { Poppins } from "next/font/google"
-import { cn } from "@/lib/utils"
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-
-const font = Poppins({subsets: ["latin"],
-weight: ["400", "600"]})
-
+const font = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
 
 export const Logo = () => {
   return (
-    <div className="hidden md:flex items-center gap-x-2"><Image alt="Logo" src="/logo.svg" width={40} height={40} />
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        alt="Logo"
+        src="/logo.svg"
+        width={40}
+        height={40}
+        className="dark:hidden"
+      />
+      <Image
+        alt="Logo"
+        src="/logo-dark.svg"
+        width={40}
+        height={40}
+        className="hidden dark:block"
+      />
+
+      <p className={cn("font-semibold", font.className)}>Jotion</p>
     </div>
-  )
-}
+  );
+};
